9dfbc75a60d1f14db09f0d52a600cb5b7681379a
Chukwu Matthew <matthewnfvcb@gmail.com>
2020-11-28 15:02:26 +0100

    updated

--- components/loading.vue
+++ components/loading.vue
@@ -1,6 +1,10 @@
 <template>
   <div v-if="loading" class="loading-page">
-    <p>Loading...</p>
+    <p class="animate-ping" text-gray-900>
+      <svg class="w-12 h-12 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+</svg>
+    </p>
   </div>
 </template>
 <script>
@@ -27,8 +31,5 @@
     height: 100%;
     text-align: center;
     padding-top: 100px;
-    color: red;
-    font-size: 40px;
-    font-family: sans-serif;
   }
 </style>
